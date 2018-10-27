3baa27ff41514428191326ef565ef21ba599485e
Rudy Ravelin <rudy@noukod.com>
2018-10-27 04:36:30 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,17 +4,16 @@ require 'pry'
 class Scraper
 
   class Scraper	class Scraper
-     def self.scrape_index_page(index_url)	
-       index_page = Nokogiri::HTML(open(index_url))	    
-      students = []	
-      index_page.css("div.roster-cards-container").each do |card|	
-        card.css(".card-text-container").each do |container|	
-          student_name = container.css(".student-name").text	
-          student_location = container.css(".student-location").text	
-          students << {name: student_name, location: student_location}	
-        end	
-      end	
-      students	
+     def self.scrape_index_page(index_url)
+       index_page = Nokogiri::HTML(open(index_url))
+      students = []
+      index_page.css("div.roster-cards-container").each do |card|
+        card.css(".card-text-container").each do |container|
+          student_name = container.css(".student-name").text
+          student_location = container.css(".student-location").text
+          students << {name: student_name, location: student_location}
+        end
+      students
     end
 
 
